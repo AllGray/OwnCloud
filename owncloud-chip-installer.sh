f81c42dccaf4a859edda7cd4115ee7c8f86e1adf
AllGray <mathiasthur@gmail.com>
2017-04-03 20:30:22 +0200

    Update owncloud-chip-installer.sh

--- owncloud-chip-installer.sh
+++ owncloud-chip-installer.sh
@@ -25,6 +25,8 @@ apt-get update
 # Install Features
 apt-get install ntfs-3g owncloud
 
+echo $?
+
 # If Apt-Get fails to run completely the rest of this isn't going to work...
 if [ $? -ne 0 ]
 then
