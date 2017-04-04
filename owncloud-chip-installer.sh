52786047bd512490491ed772e1a9fd883bd59735
AllGray <mathiasthur@gmail.com>
2017-04-04 17:08:53 +0200

    Update owncloud-chip-installer.sh

--- owncloud-chip-installer.sh
+++ owncloud-chip-installer.sh
@@ -14,6 +14,9 @@ apt-key add - < Release.key
 sh -c "echo 'deb http://download.owncloud.org/download/repositories/stable/Debian_8.0/ /' > /etc/apt/sources.list.d/owncloud.list"
 apt-get update
 
+# Install Apache2
+apt-get install apache2
+
 # Install Features
 apt-get install ntfs-3g owncloud
 
